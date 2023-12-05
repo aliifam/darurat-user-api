cc7686bf41005d27f6fb93309567ecf51c065508
aliifam <aliif.arief.m@gmail.com>
2023-12-05 14:49:49 +0700

    sspecify db port

--- src/config/db.js
+++ src/config/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const dbConfig = {
   host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASS || "password",
   database: process.env.DB_NAME || "nama_database",
